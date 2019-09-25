4c3276437a1358a4166800705a0ee00100047c2d
AlaCrity <wlchen1105@gmail.com>
2019-09-24 22:13:33 -0400

    fixed the routes to the pictures

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friends = [
   {
     name: "Bailey",
-    photo: "../styling/images/Bailey.jpg",
+    photo: "/images/Bailey.jpg",
     scores: [3, 5, 4, 3, 5, 4, 5, 4, 5, 2]
   },
   {
     name: "Kobe",
-    photo: "../styling/images/Kobe.jpg",
+    photo: "/images/Kobe.jpg",
     scores: [5, 1, 2, 2, 1, 2, 1, 2, 1, 3]
   },
   {
     name: "Evie",
-    photo: "../styling/images/Evie.jpg",
+    photo: "/images/Evie.jpg",
     scores: [1, 1, 5, 5, 1, 3, 4, 1, 3, 3]
   },
   {
     name: "Sadie",
-    photo: "../styling/images/Sadie.jpg",
+    photo: "/images/Sadie.jpg",
     scores: [4, 3, 3, 1, 2, 4, 1, 3, 5, 1]
   },
   {
     name: "Milo",
-    photo: "../../styling/images/Milo.jpg",
+    photo: "/images/Milo.jpg",
     scores: [2, 2, 5, 5, 2, 1, 3, 3, 2, 5]
   }
 ];
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,14 +2,12 @@ var path = require("path");
 var express = require("express");
 var appRoute = express.Router();
 
-console.log("WHAT IS THIS: ", path.join(__dirname, "./../public/survey.html"));
-
 appRoute.get("/survey", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/survey.html"));
+  res.sendFile(path.join(__dirname, "../public/views/survey.html"));
 });
 
 appRoute.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/home.html"));
+  res.sendFile(path.join(__dirname, "../public/views/home.html"));
 });
 
 module.exports = appRoute;
